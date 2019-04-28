5bf189b3adb702966bd709f0df786bed4489a213
Simon Mazzucca <simonmazzucca@hotmail.com>
2019-04-28 15:45:00 -0400

    Added Lib and Unit Test projects

---
+++ SpotifyToolsLib/iTunes/iTunesAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyToolsLib.iTunes
+{
+    class iTunesAdapter
+    {
+        public IList<string> GetPlaylists()
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ SpotifyToolsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpotifyToolsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpotifyToolsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8838bcd8-8727-4cdb-8186-740ef7c49922")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
